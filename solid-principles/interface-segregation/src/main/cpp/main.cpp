462254925bf96589139b648e956262b59f7d0d38
Jeffrey Cave <jcavejr@gmail.com>
2019-04-03 17:14:54 -0400

    uncommented main method because CMake can build multiple executables

--- solid-principles/interface-segregation/src/main/cpp/main.cpp
+++ solid-principles/interface-segregation/src/main/cpp/main.cpp
@@ -4,7 +4,7 @@
 #include "Printable.hpp"
 #include "Rectangle.hpp"
 #include "Square.hpp"
-/*
+
 int main(int argc, char* argv[]) {
     Rectangle rect(2.0, 3.4);
     rect.printInfo();
@@ -25,4 +25,3 @@ int main(int argc, char* argv[]) {
     a = &square;
     std::cout << "AREA: " << a->getArea() << std::endl;
 }
-*/
